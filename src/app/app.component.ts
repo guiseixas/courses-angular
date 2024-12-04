93116a6ae708b23086c3d301bc9c79c179969d3a
Guilherme Seixas <guilhermesantoseixas@outlook.com>
2024-12-04 17:42:05 -0300

    added toolbar and created courses component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [MatToolbarModule, RouterOutlet],
 })
 export class AppComponent {
   title = 'courses-angular';
